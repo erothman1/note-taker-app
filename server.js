060920d591cce8efb9c2be0b20209ec2ba377550
Elva Rothman <erothman@tulane.edu>
2023-04-18 22:01:22 -0400

    wrote if statement to write note

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express()
 
 //middleware 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 //HTML route to return notes.html
-app.get("/notes", (req, res) => 
+app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 )
 
@@ -28,12 +28,43 @@ app.get("/api/notes", (req, res) => {
     res.json(`${req.method} request received to get notes`)
 
     console.info(`${req.method} request received to get notes`)
-    
+
     //more code here?
 })
 
 //API route to receive new note to save on request body, add it to db.json. and return new note to client
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note`)
-    
+
+    const { title, text } = req.body
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuid(),
+        }
+
+
+        const noteString = JSON.stringify(newNote)
+
+        writeFile("./db/db.json", noteString, (err) =>
+            err ? console.error(err) : console.log("Note has been written to JSON file🤓")
+        )
+
+        const response = {
+            status: "success",
+            body: newNote,
+        }
+
+        console.log(response)
+        res.status(201).json(response)
+    } else {
+        res.status(500).json("Error writing note")
+    }
+
 })
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+)
