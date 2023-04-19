5d7480fd13a1d118c96f124463a300b6b4e15cc8
Elva Rothman <erothman@tulane.edu>
2023-04-18 21:42:59 -0400

    added html routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express")
 const path = require("path")
-const fs = require("fs")
-const uuid = require("uuid")
+const { writeFile, readFile } = require("fs")
+const uuid = require("uuid") //generates unique ids
 
 const PORT = 3001
 
 const app = express()
 
+//middleware 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static("public"))
+
+//HTML route to return notes.html
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+)
+
+//HTML route to return to index.html
+//wildcard path
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+)
